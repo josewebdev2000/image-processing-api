55688f2d1c7108185cf464fa6c0f7586cdd873be
josewebdev2000 <josewebdev@proton.me>
2024-07-31 07:44:48 -0400

    Installed and Configured Jasmine for Testing

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var myName = "Jose";
-var hello = function (username) { return "hello, ".concat(username); };
-console.log(hello(myName));
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,5 @@
+import myFunc from "../index";
+
+it("expects myFunc(5) to be equal to 25", () => {
+    expect(myFunc(5)).toEqual(25);
+});
